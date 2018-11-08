8ff7f63a6780c0ff1fd33949aab3cf16c1e2534f
ikechukwuhenry <ikechukwuhenry@ymail.com>
2018-11-08 12:57:14 +0100

    update global models

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-Tracking file by folder pattern:  migrations
+# Tracking file by folder pattern:  migrations
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
