5a518da015c0be260cabceb04e6bc731d1381d59
ikechukwu michael <ik@ikechukwus-Mac-Pro.local>
2019-04-02 23:59:25 +0100

    update wsgi settings to staging

--- youdecide/wsgi.py
+++ youdecide/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youdecide.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youdecide.settings.staging")
 
 application = get_wsgi_application()
 
