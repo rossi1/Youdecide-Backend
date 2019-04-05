2c9ab985095a38bc278a8fd076d600ce425b7a57
Emmanuel <emmaneulukwuegbu2016@gmail.com>
2019-04-04 19:06:54 -0700

    updated model field

--- anonymous_user/models.py
+++ anonymous_user/models.py
@@ -1,14 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 
 
 # Create your models here.
 # to detect and ignore "duplicate" votes instead(i.e.votes for the same
 # option from the same ip and browser combination for certain time period may be considered "cheated").
 class AnonymousUserModel(models.Model):
-    anonymous_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_agent = models.CharField(max_length=50)
-    user_browser = models.CharField(max_length=100)
-    user_ip = models.CharField(max_length=100)
-    user_timezone = models.CharField(max_length=100)
+    user_ip = models.GenericIPAddressField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_created',)
+
+    def __str__(self):
+        return str(self.user_ip)
+
+   
 
