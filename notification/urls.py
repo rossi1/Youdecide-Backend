ad153aaa4f5c927a31b5ec89efadf2e6b5469a5a
Emmanuel <emmaneulukwuegbu2016@gmail.com>
2019-04-17 20:23:30 -0700

    initial commit

---
+++ notification/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.NotificationUnreadListView.as_view(), name='unread'),
+    path('mark-as-read/<slug:slug>/', views.mark_as_read, name='mark_as_read'),
+    path('mark-all-as-read/', views.mark_all_as_read, name='mark_all_read'),
+    path('^latest-notifications/', views.get_latest_notifications, name='latest_notifications'),
+]
--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import get_object_or_404,
+from django.utils.translation import ugettext_lazy as _
+
+
+from rest_framework.generics import ListAPIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Notification
+
+
+class NotificationUnreadListView(ListAPIView):
+    """Basic ListView implementation to show the unread notifications for
+    the actual user"""
+    queryset = Notification
+    permission_classes = (IsAuthenticated,)
+    
+
+    def get_queryset(self, **kwargs):
+        return self.request.user.notifications.unread()
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def mark_all_as_read(request):
+    """View to call the model method which marks as read all the notifications
+    directed to the actual user."""
+    request.user.notifications.mark_all_as_read()
+    
+    msg = "All notifications to {} have been marked as read.".format(str(request.user.username))
+
+    return Response(data=msg, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def mark_as_read(request, slug=None):
+    """View to call the model method which mark as read the provided
+    notification."""
+    if slug:
+        notification = get_object_or_404(Notification, slug=slug)
+        notification.mark_as_read()
+
+    msg = "The notification {} has been marked as read.".format(notification.slug)
+  
+
+    return Response(data=msg, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_latest_notifications(request):
+    notifications = request.user.notifications.get_most_recent()
+    recent_notify = [notifs for notifs in notifications]
+    return Response(data=recent_notify, status=status.HTTP_200_OK)
