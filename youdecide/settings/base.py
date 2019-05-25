ecbae9554eb00be759cb99a68cae2abee3c8ea1f
Emmanuel <emmaneulukwuegbu2016@gmail.com>
2019-05-25 11:08:50 -0700

    whitenoise integrations

--- youdecide/settings/base.py
+++ youdecide/settings/base.py
@@ -121,6 +121,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,6 +133,9 @@ MIDDLEWARE = [
 
 # enable CORS for all domains
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Whitenoise 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'youdecide.urls'
--- youdecide/wsgi.py
+++ youdecide/wsgi.py
@@ -11,8 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youdecide.settings.staging")
 
 application = get_wsgi_application()
+
 
 # WSGIPassAuthorization On
